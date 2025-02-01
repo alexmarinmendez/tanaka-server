e636d7088f1e94d8d65453f54be925018da2ab0b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2025-02-01 17:06:05 -0500

    feat: implements custom method with JPQL QUERY

--- src/main/java/edu/galaxy/tanaka/repositories/ProductRepository.java
+++ src/main/java/edu/galaxy/tanaka/repositories/ProductRepository.java
@@ -17,4 +17,7 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
 	@Query(value = "SELECT * FROM tanaka.tbl_products WHERE STATE=1;", nativeQuery = true)	//SQL
 	List<Product> findAllCustomSQL();
 	
+	@Query(value = "SELECT p FROM Product p WHERE p.state=true")	//jpql
+	List<Product> findAllCustomJPQL();
+	
 }
