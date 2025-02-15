f646cd4f9eeeabc1b3d5342d177afa54cb4c2677
Alex Marin Mendez <alexmarinmendez@gmail.com>
2025-02-15 16:19:03 -0500

    feat: add HomeController

---
+++ src/main/java/edu/galaxy/tanaka/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package edu.galaxy.tanaka.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping
+	public String home() {
+		return "Tanaka Server App";
+	}
+	
+}
